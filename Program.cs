6d86a8fb6359f9147e5bb88afa2931c83027dc2a
Blazingfury05 <82287955+Blazingfury05@users.noreply.github.com>
2026-09-24 03:47:17 -0400

    Modify age values and calculate total balance
    Updated age variables and added account balance calculations.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
-int age = 30;
-int age2 = 5;
+Console.WriteLine("Hello, World!");
+int age = 21;
+int age2 = 14;
 
-Console.WriteLine("I am " + age + " and my sibling is " + age2);
+Console.WriteLine("I am " + age + " years old, and my brother is " + age2 + " years old");
 
-Console.WriteLine("my mom was " + (age + 2) + " when she had me");
+Console.WriteLine(age * age2);
+
+#define check_account = 239.49;
+#define saving_account = 1310.60;
+
+define total_balance = check_account + saving_account;
+Console.WriteLine(total_balance);
