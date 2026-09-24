fc065090b1328c47c89c8b677820e2880fd55091
Blazingfury05 <82287955+Blazingfury05@users.noreply.github.com>
2026-09-24 03:46:09 -0400

    Basic structure practice before implimentation

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+int age = 30;
+int age2 = 5;
+
+Console.WriteLine("I am " + age + " and my sibling is " + age2);
+
+Console.WriteLine("my mom was " + (age + 2) + " when she had me");
